1fe39975d4dc92f9cd78282b70e9eac20e449700
pseudness <pseudness@gmail.com>
2021-05-03 18:18:56 +0200

    User Implemented

---
+++ user.java
@@ -0,0 +1,42 @@
+/*
+*
+*  @Author Eyad Mostafa
+*
+*/
+
+public class User{
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    public User(String username, String password, String fname, String lname) {
+        this.username = username;
+        this.password = password;
+        firstName = fname;
+        lastName = lname;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setFirstName(String fname) {
+        firstName = fname;
+    }
+    public void setLastName(String lname) {
+        lastName = lname;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getFirstName() {
+        return FirstName;
+    }
+    public String getLastName() {
+        return LastName;
+    }
+}
